d588a94e6df733706e40f0eb17073e8212823585
rafael <rafaeelopes20@gmail.com>
2025-07-30 21:20:30 -0300

    aula02

---
+++ Eclipse-Workspace/n25_java2-01/src/br/com/senaisp/bauru/secao02/aula03/ArtAscii02.java
@@ -0,0 +1,21 @@
+package br.com.senaisp.bauru.secao02.aula03;
+
+public class ArtAscii02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("      O\r\n"
+     		+ "         _|_\r\n"
+     		+ "   ,_.-_' _ '_-._,\r\n"
+     		+ "    \\ (')(.)(') /\r\n"
+     		+ " _,  `\\_-===-_/`  ,_\r\n"
+     		+ ">  |----\"\"\"\"\"----|  <\r\n"
+     		+ "`\"\"`--/   _-@-\\--`\"\"`\r\n"
+     		+ "     |===L_I===|\r\n"
+     		+ "      \\       /\r\n"
+     		+ "      _\\__|__/_\r\n"
+     		+ " jgs `\"\"\"\"`\"\"\"\"`");
+    
+	}
+
+}
